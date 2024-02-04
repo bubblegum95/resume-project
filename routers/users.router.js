d9e793117d17e57870cd205f3332368df9fea07f
Bubblegum95 <a82727282@gmail.com>
2024-02-04 18:38:03 +0900

    fix: 로그인 api

--- routers/users.router.js
+++ routers/users.router.js
@@ -24,17 +24,63 @@ router.post('/sign-up', async (req, res) => {
     return res.status(400).json({success: false, message: "이름은 필수입니다."})
   }
 
+  if (password.length < 6) {
+    return res.status(400).json({success: false, message: "비밀번호는 최소 6자 이상입니다."})
+  }
+
   if (password !== passwordConfirm) {
     return res.status(400).json({success: false, message: "비밀번호가 일치하지 않습니다."})
   }
 
-  const user = await prisma.user.findFirst({
+  const user = await prisma.users.findFirst({
     where: {
       email, 
-
     }
+  })
+
+  if (user) {
+    return res.status(400).json({success: false, message: "이미 존재하는 이메일입니다."})
+  }
+
+  await prisma.users.create({
+    data: {
+      email,
+      password, 
+      name,
+    }
+  })
+
+  return res.status(201).json({
+    email,
+    name,
   })
 })
 
+router.post('/sign-in', async (req, res) => {
+  const {email, password} = req.body;
+  if (!email) {
+    return res.status(400).json({success: false, message: "이메일을 필수입니다."})
+  }
+
+  if (!password) {
+    return res.status(400).json({success: false, message: "비밀번호는 필수입니다."})
+  }
+
+  const user = await prisma.users.findFirst({
+    where: {
+      email,
+      password,
+    }
+  })
+
+  if (!user) {
+    return res.status(401).json({success: false, message: "이메일 또는 비밀번호가 올바르지 않습니다."})
+  }
+
+  const accessToken = jwt.sign({userId: user.userId}, 'resume@@', {expiresIn: '12h'})
+  return res.json({
+    accessToken,
+  })
+})
 
 export default router;
