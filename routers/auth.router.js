f489892edf9b75fd0e4dd39dd6c253693b422fea
Bubblegum95 <a82727282@gmail.com>
2024-02-06 00:12:41 +0900

    fix: 토큰 재발급 오류

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -8,30 +8,15 @@ dotenv.config();
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.get('/tokens', async (req, res)=>{
+router.post('/tokens', async (req, res)=>{
   const {refreshToken} = req.body; 
-
-  if(!refreshToken) {
-    throw new Error('인증 정보가 올바르지 않습니다.')
-  }
-
-  const {tokenType, tokenValue} = refreshToken.split(" ");
-  if(tokenType !== 'Bearer') {
-    throw new Error('토큰 형식이 Bearer가 아닙니다.')
-  }
-
-  if(!tokenValue) {
-    throw new Error('인증 정보가 올바르지 않습니다.')
-  }
-
-  const token = jwt.verify(tokenValue, process.env.REFRESH_TOKEN_KEY); //refreshToken 유효성 검사
-  if (!token.userId) {
-    return res.status(401).end();
+  if (!refreshToken) {
+    return res.status(401).json({message: "권한이 없습니다."});
   }
 
   const user = await prisma.users.findFirst({
     where: {
-      userId: token.userId, //Users 테이블의 userId에서 token의 userId와 일치하는 값을 찾아라
+      userId: refreshToken.userId, //Users 테이블의 userId에서 token의 userId와 일치하는 값을 찾아라
     }
   })
 
